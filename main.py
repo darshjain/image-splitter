c93493ceabf174b64a3d843aa66c22fec6ce5f8a
darshjain <darshj786@gmail.com>
2021-01-21 23:02:39 +0530

    dow

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ import tkinter as tk
 import os
 from PIL import Image
 
-rows=columns=0
+rows = columns = 0
 
 
-#window
-window=tk.Tk()
+# window
+window = tk.Tk()
 window.geometry("500x500")
-window.grid_columnconfigure((0,1), weight=1)
-
+window.grid_columnconfigure((0, 1), weight=1)
 window.title("Image Splitter")
+
+# buttons
+add_file = tk.Button(
+    text="ADD FILE",
+    width=70,
+    height=3,
+    # command=generate1,
+    font=('Helvetica', 10, 'bold'),
+)
+add_file.grid(column=0,row=0)
+
 window.mainloop()
