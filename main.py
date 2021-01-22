63ac27e4901ad04b55f212da321074a0691bf016
darshjain <darshj786@gmail.com>
2021-01-22 10:17:22 +0530

    Image Preview

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ def add_photo_dialog():
     global photo
     filename = filedialog.askopenfilename()
     photo = tk.PhotoImage(file=filename)
+    update_photo()
+def update_photo():
+    label_display["image"]=photo
 
-# window
+#------------------ window---------------------
 window = tk.Tk()
-window.geometry("500x500")
+window.geometry("700x700")
 window.grid_columnconfigure((0, 1), weight=1)
 window.title("Image Splitter")
 
-# buttons
+#------------------buttons---------------------
 add_file = tk.Button(
     text="ADD FILE",
     width=70,
@@ -24,6 +27,14 @@ add_file = tk.Button(
     command=add_photo_dialog,
     font=('Helvetica', 10, 'bold'),
 )
-add_file.grid(column=0,row=0)
+label_display=tk.Label(
+    text="",
+    image="",
+    width=500,
+    height=500,
+    border=4,
+)
 
+add_file.grid(column=0,row=0)
+label_display.grid(column=0,row=2)
 window.mainloop()
