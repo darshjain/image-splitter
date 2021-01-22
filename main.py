2cbf964984e504872bfde467e5d798dbd59ae23b
darshjain <darshj786@gmail.com>
2021-01-22 11:32:11 +0530

    WORKING-IMAGE RESIZE

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import tkinter as tk
+from tkinter import *
 import os
-from PIL import Image
+from PIL import Image, ImageTk
 from tkinter import filedialog
 
 rows = columns = 3
 
-def add_photo_dialog():
-    global photo
-    filename = filedialog.askopenfilename()
-    photo = tk.PhotoImage(file=filename)
-    update_photo()
-def update_photo():
-    label_display["image"]=photo
 
-#------------------ window---------------------
+def add_photo_dialog():
+    global photo_selected
+    filename = filedialog.askopenfilename()
+    
+    image=Image.open(filename)
+    image = image.resize((450, 350), Image.ANTIALIAS)
+    #my_img = ImageTk.PhotoImage(image)
+
+#   resized_image=image.resize((500,500),Image.ANTIALIAS)
+    photo_selected = ImageTk.PhotoImage(image)
+    label_display["image"]=photo_selected
+
+
+# ------------------ window---------------------
 window = tk.Tk()
 window.geometry("700x700")
 window.grid_columnconfigure((0, 1), weight=1)
 window.title("Image Splitter")
 
-#------------------buttons---------------------
+# ------------------buttons---------------------
 add_file = tk.Button(
     text="ADD FILE",
     width=70,
@@ -27,7 +34,7 @@ add_file = tk.Button(
     command=add_photo_dialog,
     font=('Helvetica', 10, 'bold'),
 )
-label_display=tk.Label(
+label_display = tk.Label(
     text="",
     image="",
     width=500,
@@ -35,6 +42,6 @@ label_display=tk.Label(
     border=4,
 )
 
-add_file.grid(column=0,row=0)
-label_display.grid(column=0,row=2)
+add_file.grid(column=0, row=0)
+label_display.grid(column=0, row=2)
 window.mainloop()
