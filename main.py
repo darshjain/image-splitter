27098512e2481b6d731b1902d229a049be5b82b1
darshjain <darshj786@gmail.com>
2021-01-24 10:46:51 +0530

    Try except block added and message box implemented

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import *
 import os
 from PIL import Image, ImageTk
-from tkinter import filedialog
+from tkinter import filedialog,messagebox
 
 global rows, columns
 rows = columns = 3
@@ -33,22 +33,28 @@ def add_photo_dialog():
 
 
 def image_crop(input, x_pieces, y_pieces):
-    filename, file_extension = os.path.splitext(input)
-    image_handle = Image.open(input)
-    image_width, image_height = image_handle.size
-    height = image_height // y_pieces
-    width = image_width // x_pieces
-    for i in range(0, y_pieces):
-        for j in range(0, x_pieces):
-            box = (j * width, i * height, (j + 1) * width, (i + 1) * height)
-            out = os.path.join("Output//", f'{i}_{j}{file_extension}')
-            image_handle.crop(box).save(out)
-
+    try:
+        filename, file_extension = os.path.splitext(input)
+        image_handle = Image.open(input)
+        image_width, image_height = image_handle.size
+        height = image_height // y_pieces
+        width = image_width // x_pieces
+        for i in range(0, y_pieces):
+            for j in range(0, x_pieces):
+                box = (j * width, i * height, (j + 1) * width, (i + 1) * height)
+                out = os.path.join("Output//", f'{i}_{j}{file_extension}')
+                image_handle.crop(box).save(out)
+                tk.messagebox.showinfo(title="Successful",message="Images saved in Output folder")
+    except:
+        tk.messagebox.showinfo(title="Error",message="Error While Generating images")
 
 def divide_photo():
-    rows = int(rows_textbox.get())
-    columns = int(columns_textbox.get())
-    image_crop(filename, rows, columns)
+    try:
+        rows = int(rows_textbox.get())
+        columns = int(columns_textbox.get())
+        image_crop(filename, rows, columns)
+    except:
+        tk.messagebox.showinfo(title="Error",message="Please Enter Values of rows and columns")
 
 
 # ------------------Components---------------------
